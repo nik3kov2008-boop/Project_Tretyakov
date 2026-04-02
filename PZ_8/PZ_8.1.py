d02bf6de9ee06331c01c578022df8e99e29e0d38
Nikita <nik3kov2008@gmail.com>
2026-04-02 22:57:16 +0300

    Сделал 8 практос

---
+++ PZ_8/PZ_8.1.py
@@ -0,0 +1,8 @@
+# Создать словарь my_dict = {'name': 'Alice', 'age': 35, 'city': 'New York'}. Добавьте пару
+# 'profession': 'Doctor', измените age на 40 и выведите значение city.
+
+my_dict = {'name': 'Alice', 'age': 35, 'city': 'New York'}
+
+my_dict['profession'] = 'Doctor'
+my_dict['age'] = 40
+print(my_dict['city'])
